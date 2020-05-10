d5b6955a0e91606bc102c86413a1ac1bfccad38d
Tim Zehr <timzehr@dzehr.com>
2020-05-10 06:40:35 -0400

    making change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WHAT UP???
+          Change on feature branch
         </p>
         <a
           className="App-link"
